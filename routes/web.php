f413d838eb58f0a240f10aa9a65e3a2494938a18
MOULOUNGUI-Samuel <moulounguibienvenuesamara@gmail.com>
2025-08-18 14:35:28 +0200

    okokokok

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard',  [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
